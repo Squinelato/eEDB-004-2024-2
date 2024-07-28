cabf7bbf8431b4a6a310d396c547981fc6632f20
João Squinelato - Via Consulting <35571896+Squinelato@users.noreply.github.com>
2024-07-28 01:29:48 -0300

    feat: adding ship delay analysis

--- multidimensional_model/question_6.sql
+++ multidimensional_model/question_6.sql
@@ -1,16 +1,24 @@
-WITH delivery AS (
-	SELECT
-		dw_dlv.OrderID AS order_id,
-		dw_dlv.RequiredDate AS required_date,
-		dw_dlv.ShippedDate AS shipped_date,
-        DATEDIFF(dw_dlv.RequiredDate, dw_dlv.ShippedDate) AS advance_ship_rate_in_days
-	FROM mydb.dw_delivery as dw_dlv
-), delivery_metrics AS (
-	SELECT
-		AVG(advance_ship_rate_in_days) AS avg_advance_ship_rate_in_days,
-        STD(advance_ship_rate_in_days) AS std_advance_ship_rate_in_days
-	FROM delivery
-)
-SELECT * FROM delivery_metrics;
+CREATE OR REPLACE VIEW mydb.view_advance_ship_rate_in_days AS
+SELECT
+	dw_dlv.OrderID AS order_id,
+	dw_dlv.RequiredDate AS required_date,
+	dw_dlv.ShippedDate AS shipped_date,
+	DATEDIFF(dw_dlv.RequiredDate, dw_dlv.ShippedDate) AS advance_ship_rate_in_days
+FROM mydb.dw_delivery as dw_dlv
+WHERE dw_dlv.ShippedDate IS NOT NULL;
 
-SELECT * FROM mydb.dw_delivery;
+CREATE OR REPLACE VIEW mydb.view_advance_ship_rate_in_days_percentiles AS
+SELECT DISTINCT
+	advance_ship_rate_in_days,
+	ROUND((CUME_DIST() OVER(ORDER BY advance_ship_rate_in_days ASC) * 100 ), 2) AS percentile
+FROM mydb.view_advance_ship_rate_in_days;
+
+CREATE OR REPLACE VIEW view_advance_ship_rate_in_days_mean_and_std AS
+SELECT
+	ROUND(AVG(advance_ship_rate_in_days), 2) AS avg_advance_ship_rate_in_days,
+	ROUND(STD(advance_ship_rate_in_days), 2) AS std_advance_ship_rate_in_days
+FROM mydb.view_advance_ship_rate_in_days;
+
+SELECT * FROM mydb.view_advance_ship_rate_in_days;
+SELECT * FROM mydb.view_advance_ship_rate_in_days_percentiles;
+SELECT * FROM mydb.view_advance_ship_rate_in_days_mean_and_std;
